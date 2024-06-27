1df92a474ca27a42fd91ac54b531ae2ec1543622
DeShon Dixon <deshondixon@gmail.com>
2024-06-27 12:23:31 -0500

    Initial work on AlarmClock

---
+++ Scratch/src/AlarmClockClient.java
@@ -0,0 +1,28 @@
+/*
+ * Client-side "main-class" i.e., the class definition with main().
+ * That's the ONLY thing in here.
+ * We will create a few AlarmClock objects and give them a basic test-drive.
+ */
+class AlarmClockClient {
+
+    // starting point or "entry" point for every standalone application
+    public static void main(String[] args) {
+        // create an instance (or object) of AlarmClock and set its snoozeInterval
+        AlarmClock clock1 = new AlarmClock();
+        clock1.snoozeInterval = 7;
+
+
+        // create a 2nd AlarmClock and set its snoozeInterval, also
+        AlarmClock clock2 = new AlarmClock();
+        clock2.snoozeInterval = 10;
+
+        // create a 3rd AlarmClock object, but this time DON'T give it a snoozeInterval
+        AlarmClock clock3 = new AlarmClock();
+
+        // make them snooze()
+        clock1.snooze();
+        clock2.snooze();
+        clock3.snooze();
+
+    }
+}
