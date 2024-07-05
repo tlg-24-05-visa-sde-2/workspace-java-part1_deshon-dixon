0ba1073794d9b70fc35ffb3a50c831642d65e98c
DeShon Dixon <deshondixon@gmail.com>
2024-07-05 14:27:37 -0500

    StringTest Examples

--- Scratch/src/Movie.java
+++ Scratch/src/Movie.java
@@ -68,7 +68,7 @@ class Movie {
     }
 
     public String toString() {
-        return String.format("Movie: title=%s, releaseYear=%s, revenue=%s, rating=%s, genre=%s", getTitle(), getReleaseYear(), (getRevenue() == null) ? "N/A" : String.format("%,.2f", getRevenue()), getRating(), getGenre());
+        return String.format("Movie: title=%s, releaseYear=%s, revenue=%s, rating=%s, genre=%s", getTitle(), getReleaseYear(), (getRevenue() == null) ? "null" : String.format("%,.2f", getRevenue()), getRating(), getGenre());
 
 //        return "Movie: title= " + getTitle() +
 //                ", releaseYear= " + getReleaseYear() +
