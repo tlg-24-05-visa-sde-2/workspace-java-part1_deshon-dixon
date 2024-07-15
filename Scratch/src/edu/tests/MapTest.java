362084c9439f56fb664a1dd20b6f10b1b5932f99
DeShon Dixon <deshondixon@gmail.com>
2024-07-15 15:06:15 -0500

    Fun with Maps

---
+++ Scratch/src/edu/tests/MapTest.java
@@ -0,0 +1,48 @@
+package edu.tests;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+class MapTest {
+
+    public static void main(String[] args) {
+        Map<String, Double> gpaMap = new HashMap<>();            // K: String   V: Double
+
+        gpaMap.put("kea", 4.0);
+        gpaMap.put("jasmin", 3.6);
+        gpaMap.put("kelvin", 1.7);
+        gpaMap.put("deshon", 2.5);
+        gpaMap.put("sherie", 3.84);
+        gpaMap.put("tyler", 1.9);
+
+        Double jasminGpa = gpaMap.get("jasmin");
+        System.out.println("Jasmin's GPA was: " + jasminGpa);
+        System.out.println();
+
+        dump(gpaMap);
+        System.out.println();
+
+        // print all gpa's that are 3.5 or greater
+        Collection<Double> gpas = gpaMap.values(); // returns Collections<V>, i.e., Collection<Double>
+        for (Double gpa : gpas) {
+            if (gpa >= 3.5) {
+                System.out.println(gpa);
+            }
+        }
+        System.out.println();
+
+        // print all names of those with a gpa of 2.5 or greater
+        for (var entry : gpaMap.entrySet()) {
+            if (entry.getValue() >= 2.5) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+
+    public static void dump(Map<String, Double> gpaMap) {
+        for (Map.Entry<String, Double> entry : gpaMap.entrySet()) {
+            System.out.println(entry.getKey() + " | " + entry.getValue());
+        }
+    }
+}
